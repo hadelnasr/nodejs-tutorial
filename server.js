8503efb5af5d54f921524092fd5c9932b1750115
hadelnasr <haytham_einshams@hotmail.com>
2022-08-15 00:26:29 +0200

    adding nodemon dev dependencies

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const friends = [
+    {
+        id: 0,
+        name: "Haytham adel",
+    },
+    {
+        id: 1,
+        name: "Mostafa Haytham adel",
+    }
+];
+
 app.get('/', (req, res) => {
     res.send('Hellloooooo');
+})
+
+app.get('/friends/:id', (req, res) => {
+    const friendId = req.params.id;
+    friends[friendId] ? res.json(friends[friendId]) : res.sendStatus(404);
 })
 
 app.get('/messages', (req, res) => {

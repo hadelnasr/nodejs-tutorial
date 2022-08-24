c2b8c1edb76045e04e56d5f948c16162178f4999
haytham adel <haytham_einshams@hotmail.com>
2022-08-24 14:36:50 +0200

    middlewares in express

--- server.js
+++ server.js
@@ -13,8 +13,19 @@ const friends = [
     }
 ];
 
+app.use((req, res, next) => {
+    const start = Date.now();
+    console.log(`${req.method} ${req.url}`);
+    next();
+    const end = Date.now();
+    console.log(end - start);
+})
+
+// used to parse the response
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('Hellloooooo');
+    res.send('Hellloooooo haitham');
 })
 
 app.get('/friends/:id', (req, res) => {
@@ -25,8 +36,21 @@ app.get('/friends/:id', (req, res) => {
 app.get('/messages', (req, res) => {
     res.send('<ul><li>Hellow from messages</li></ul>');
 })
+
 app.post('/messages', (req, res) => {
     console.log("Updating messages ...");
+})
+
+app.post('/friends', (req, res) => {
+    if (!req.body.name) {
+        return res.sendStatus(400)
+    }
+    const newFriend = {
+        id: friends.length,
+        name: req.body.name
+    }
+    friends.push(newFriend);
+    res.json(newFriend);
 })
 
 app.listen(PORT, () => {

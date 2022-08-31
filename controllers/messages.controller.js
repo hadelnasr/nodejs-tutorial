d2a0d318f6fd323de6e255c38a915cd951b17e73
haytham adel <haytham_einshams@hotmail.com>
2022-08-31 16:37:24 +0200

    working on layouts and views

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 function getMessages(req, res) {
-    res.send('<ul><li>Hellow from messages controller</li></ul>');
+    res.sendFile(path.join(__dirname, '..', 'public', 'mts-logo.jpg'));
+    // res.send('<ul><li>Hellow from messages controller</li></ul>');
 };
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const friendsRouter = require('./routes/friends.router');
 const messagesRouter = require('./routes/messages.router');
@@ -17,12 +18,23 @@ app.use((req, res, next) => {
 // used to parse the response
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hellloooooo haitham');
-})
+// app.get('/', (req, res) => {
+//     res.send('Hellloooooo haitham');
+// })
 
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
+app.use('/', (req, res) => {
+    res.render('index', {
+        title: 'My friends',
+        header: 'this is the header',
+        paragraph: 'this is the paragraph as well'
+    })
+})
+
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
